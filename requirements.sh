aa6e0a17a5cf0e53f426e70fdecdbbfdd0bb0d99
jebin2 <jebineinstein@gmail.com>
2024-10-18 14:35:56 +0530

    requirement file added.

---
+++ requirements.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Update package list and install required packages
+sudo apt update
+
+# Install required packages for the project
+sudo apt install -y \
+    python3 \
+    python3-pip \
+    ffmpeg \
+    libglib2.0-0 \
+    libsm6 \
+    libxext6 \
+    libxrender1 \
+    libfreetype6-dev \
+    libharfbuzz-dev \
+    ttf-dejavu \
+    sqlite3 \
+    python3-opencv
